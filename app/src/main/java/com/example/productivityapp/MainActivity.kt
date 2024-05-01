fde0d1851e60bf6aa72c9a4554a6804ab6f98570
Eisen <eisengong@gmail.com>
2024-05-01 11:00:27 -0700

    whatt he fricj

--- app/src/main/java/com/example/productivityapp/MainActivity.kt
+++ app/src/main/java/com/example/productivityapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.productivityapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.productivityapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.chronometerMainStopwatch.isCountDown = true
+
     }
 }
