aefcb1daa0dd18eeaa3010020cf2ad5dd5403a64
Eisen <eisengong@gmail.com>
2024-05-09 10:06:57 -0700

    i give up :(

--- app/src/main/java/com/example/productivityapp/MemoryPi.kt
+++ app/src/main/java/com/example/productivityapp/MemoryPi.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.productivityapp.databinding.ActivityMemoryPiBinding
 import java.io.FileNotFoundException
+import kotlin.math.max
 
 
 class MemoryPi : AppCompatActivity() {
@@ -16,6 +17,7 @@ class MemoryPi : AppCompatActivity() {
     private lateinit var binding: ActivityMemoryPiBinding
 
     private var newTextToMemorize = ""
+
     private lateinit var fullText: String
     private var numRemoved = 0
 
@@ -44,6 +46,18 @@ class MemoryPi : AppCompatActivity() {
         }
 
         fullText = binding.textViewMemoryPiTextMemorize.text.toString()
+        Log.d(TAG, "onCreate: $fullText")
+
+        val fullTextSplit = fullText.split(" ").toMutableList()
+        Log.d(TAG, "onCreate: $fullTextSplit")
+
+        var max = 0
+
+        for (i in fullTextSplit.indices) {
+            max = max(max, fullTextSplit[i].length)
+        }
+
+        val choppedFullText = fullTextSplit
 
 
         binding.buttonMemoryPiEditText.setOnClickListener {
@@ -60,7 +74,6 @@ class MemoryPi : AppCompatActivity() {
                     newTextToMemorize = input.text.toString()
                     writeToStorage(newTextToMemorize)
                     binding.textViewMemoryPiTextMemorize.text = newTextToMemorize
-
                 }
             )
             builder.setNegativeButton("Cancel", null)
@@ -69,17 +82,37 @@ class MemoryPi : AppCompatActivity() {
         }
 
         binding.buttonMemoryPiAddLetter.setOnClickListener {
-            var tempText =
+            for (i in 0 until choppedFullText.size) {
+                if (numRemoved <= fullTextSplit[i].length)
+                    choppedFullText[i] = fullTextSplit[i].substring(0,numRemoved+1) + "_".repeat(numRemoved+1)
+                }
+            if (numRemoved >= 0) {
+                numRemoved--
+            }
+
+            binding.textViewMemoryPiTextMemorize.text = choppedFullText.joinToString(" ")
 
 
         }
 
         binding.buttonMemoryPiRemoveLetter.setOnClickListener {
-            var tempText = fullText.split(" ").toMutableList()
-            for (i in 0 until tempText.size) {
-                tempText[i] = (tempText[i].substring(0, tempText[i].length - 2))
-                numRemoved++;
+            if (numRemoved <= max) {
+                numRemoved++
             }
+
+            for (i in 0 until choppedFullText.size) {
+                if (numRemoved <= choppedFullText[i].length) {
+                    choppedFullText[i].replaceRange(
+                        choppedFullText[i].length - numRemoved - 1,
+                        choppedFullText[i].length,
+                        "_".repeat(numRemoved + 1)
+                    )
+                }
+            }
+
+
+            binding.textViewMemoryPiTextMemorize.text = choppedFullText.joinToString(" ")
+
         }
 
 
