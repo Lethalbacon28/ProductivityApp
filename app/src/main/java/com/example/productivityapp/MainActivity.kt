dbbbc4e95b7df382e378d87beec09ab24207f827
Eisen <eisengong@gmail.com>
2024-05-17 10:09:19 -0700

    making file select process

--- app/src/main/java/com/example/productivityapp/MainActivity.kt
+++ app/src/main/java/com/example/productivityapp/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.productivityapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.productivityapp.databinding.ActivityMainBinding
+import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +15,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val mydir: File = this.getDir("ProductivityApp", MODE_PRIVATE) //Creating an internal dir;
+
+        if (!mydir.exists()) {
+            mydir.mkdirs()
+        }
+
+        Log.d("Something", "onCreate: ${mydir.listFiles()}")
 
         //binding.chronometerMainStopwatch.isCountDown = true
 
---
+++ app/src/main/java/com/example/productivityapp/MemoryPiFileSelect.kt
@@ -0,0 +1,11 @@
+package com.example.productivityapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MemoryPiFileSelect : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memory_pi_file_select)
+    }
+}
